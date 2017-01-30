c63bca23bf41db7b55510b9e93eff29ff160e16f
Richard Ott <ott.richard@gmail.com>
2017-01-29 19:03:00 -0800

    Updated heroku settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python uploadPic/uploadPic.py
--- uploadPic/uploadPic.py
+++ uploadPic/uploadPic.py
@@ -54,3 +54,8 @@ def upload_file():
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
